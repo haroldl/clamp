eceefc367123924396940ed12c19636edaee9e91
Harold Hotelling <harold@hotelling.net>
2024-05-11 15:50:42 -0700

    Run Python code from Lisp.

---
+++ clamp.lisp
@@ -0,0 +1,19 @@
+#!/usr/bin/sbcl --script
+
+(defpackage "CLAMP"
+  (:use "SB-ALIEN"))
+				;
+(load-shared-object "/usr/lib/python3.11/config-3.11-x86_64-linux-gnu/libpython3.11.so")
+
+(define-alien-routine ("Py_Initialize" py-initialize) void)
+(define-alien-routine ("Py_Finalize" py-finalize) void)
+(define-alien-routine ("PyRun_SimpleString" py-run-simple-string) int (str c-string))
+
+(py-initialize)
+(unwind-protect
+     (progn
+       (write-line "protected")
+       (py-run-simple-string "print(3 + 5)"))
+  (progn
+    (py-finalize)
+    (write-line "cleaned up")))
