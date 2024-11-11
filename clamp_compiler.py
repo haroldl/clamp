07ca717495aafe5743a2347f1d04fd8c50fd78f0
Harold Hotelling <harold@hotelling.net>
2024-11-11 15:33:26 -0800

    main for clamp_compiler

--- clamp_compiler.py
+++ clamp_compiler.py
@@ -4,6 +4,7 @@
 #
 
 import ast
+import sys
 
 
 codegen_handlers = {}
@@ -73,3 +74,17 @@ def f(x):
   return x + 1
 """)
     print(v)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("You must specify the file name for the Python source to translate to Lisp.")
+        sys.exit(1)
+
+    for i in range(len(sys.argv)):
+        print(i, sys.argv[i])
+
+    with open(sys.argv[1]) as source_file:
+        source = source_file.read()
+        output = compile(source)
+        print(output)
