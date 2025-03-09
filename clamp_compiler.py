e1278cb302a1c0318f412056e3f131fd8c955484
Harold Hotelling <harold@hotelling.net>
2025-03-08 18:47:43 -0800

    Reify more context for codegen to allow return.

--- clamp_compiler.py
+++ clamp_compiler.py
@@ -1,54 +1,91 @@
-""" Python to Common Lisp Compiler
+#!/usr/bin/env python
+
+"""
+Python to Common Lisp Compiler
 """
 
 import ast
+from dataclasses import dataclass, replace
 import sys
 
 
 codegen_handlers = {}
 
 
-def codegen(node, top_level_stmt = False):
+@dataclass
+class Context():
+    top_level_stmt: bool = True
+    block_name: str | None = None
+
+    def child(self):
+        return replace(self, top_level_stmt = False)
+
+
+def codegen(node, context : Context = Context(top_level_stmt=False)):
     """Recursive codegen of Common Lisp from a Python AST"""
     typ = type(node)
     if typ in codegen_handlers:
-        return codegen_handlers[typ](node, top_level_stmt)
+        return codegen_handlers[typ](node, context)
     else:
         raise Exception(f"Do not have support to codegen {str(type(node))} node with value {ast.dump(node)}")
 
 
-def codegen_assign(node, top_level_stmt):
+def codegen_assign(node, context : Context):
     if len(node.targets) == 1:
-        return f"(setf {codegen(node.targets[0])} {codegen(node.value)})"
+        lhs = codegen(node.targets[0], context.child())
+        rhs = codegen(node.value, context.child())
+        return f"(setf {lhs} {rhs})"
     else:
         raise Exception("TODO: destructuring bind")
 
 
-def codegen_function(node, top_level_stmt):
-    params = codegen_args(node.args)
+def codegen_function(node, context : Context):
+    child_context = context.child()
+
+    params = codegen_args(node.args, child_context)
 
     # Python is a Lisp-1, Common Lisp is a Lisp-2
     # For compiled Python code running in SBCL, we'll put functions and other variables in the
     # same namespace which means we need to use funcall/apply to invoke compiled Python functions.
-    hed = f"(setf {node.name} (lambda ({params}) "
-    bod = "\n".join([codegen(n, top_level_stmt) for n in node.body])
+    hed = f"(setf {node.name} (lambda ({params}) (block {node.name} "
 
-    return hed + bod + "))\n"
+    body_context = replace(child_context, block_name=node.name)
+    bod = "\n".join([codegen(n, body_context) for n in node.body])
 
-
-def codegen_funcall(node, top_level_stmt):
-    args_str = " ".join([codegen(a, False) for a in node.args])
-    return "(funcall " + codegen(node.func, top_level_stmt) + " " + args_str + ")"
+    return hed + bod + ")))\n"
 
 
-def codegen_module(node, top_level_stmt):
+def codegen_funcall(node, context : Context):
+    child_context = context.child()
+    target = codegen(node.func, child_context)
+    args_str = " ".join([codegen(a, child_context) for a in node.args])
+    return f"(funcall {target} {args_str})"
+
+
+def codegen_module(node, context : Context):
+    child_context = context.child()
     header_code = """(in-package :clamp) (use-package "CLAMP.__builtins__")\n"""
     name_code = """(defvar __name__ "__main__")\n"""
-    body_code = "\n".join([codegen(n) for n in node.body])
+    body_code = "\n".join([codegen(n, child_context) for n in node.body])
     return (header_code + name_code + body_code)
 
 
-codegen_handlers[ast.Expr] = lambda node, top_level_stmt: codegen(node.value, top_level_stmt)
+def codegen_return(node, context : Context):
+    if not context.block_name:
+        raise Exception("Trying to return but not inside a lexical scope.")
+    retval = codegen(node.value, context.child())
+    return f"(return-from {context.block_name} {retval})"
+
+
+def codegen_binary_operator(node, context : Context):
+    child_context = context.child()
+    op = codegen(node.op, child_context)
+    lhs = codegen(node.left, child_context)
+    rhs = codegen(node.right, child_context)
+    return f"({op} {lhs} {rhs})"
+
+
+codegen_handlers[ast.Expr] = lambda node, context: codegen(node.value, context)
 codegen_handlers[ast.Assign] = codegen_assign
 codegen_handlers[ast.FunctionDef] = codegen_function
 codegen_handlers[ast.Call] = codegen_funcall
@@ -56,14 +93,14 @@ codegen_handlers[ast.Name] = lambda node, _: node.id
 codegen_handlers[ast.Module] = codegen_module
 codegen_handlers[ast.Add] = lambda node, _: "+"
 codegen_handlers[ast.Mult] = lambda node, _: "*"
-codegen_handlers[ast.BinOp] = lambda node, _: "(" + codegen(node.op) + " " + codegen(node.left) + " " + codegen(node.right) + ")"
+codegen_handlers[ast.BinOp] = codegen_binary_operator
 codegen_handlers[ast.Constant] = lambda node, _: codegen(node.value)
-codegen_handlers[ast.Return] = lambda node, top_level_stmt: codegen(node.value, top_level_stmt)
+codegen_handlers[ast.Return] = codegen_return
 codegen_handlers[int] = lambda node, _: str(node)
 codegen_handlers[float] = lambda node, _: str(node)
 codegen_handlers[str] = lambda node, _: '"' + str(node) + '"' # TODO: escape nested quotes correctly
 
-def codegen_args(args):
+def codegen_args(args, context: Context):
     #print(args.args)
     #print(args.defaults)
     #print(args.kw_defaults)
@@ -77,7 +114,7 @@ def codegen_args(args):
 def compile(code):
     print("Preparing to compile:", code)
     code_tree = ast.parse(code)
-    return codegen(code_tree, True)
+    return codegen(code_tree, Context(top_level_stmt=True))
 
 
 def demo():
@@ -89,3 +126,6 @@ def f(x):
   return x + 1
 """)
     print(v)
+
+if __name__ == '__main__':
+    demo()
